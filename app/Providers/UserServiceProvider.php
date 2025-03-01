43c588f2732234347594e82b08adfc48175faf5a
Dawn Funtilon <dawnfuntilon@Windows-1220.local>
2025-03-01 18:15:52 +0800

    Exercise #3 - Routing, Middleware and CSRF

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -15,14 +15,21 @@ class UserServiceProvider extends ServiceProvider
         $this->app->singleton(UserService::class, function ($app) {
             $users = [
                 [
+                    'id' => 1,
                     'name' => 'John Doe',
                     'gender' => 'Male'
                 ],
                 [
+                    'id' => 2,
                     'name' => 'Jane Doe',
                     'gender' => 'Female'
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'Sample Test',
+                    'gender' => 'Female'
                 ]
-                ];
+            ];
 
             return new UserService($users); 
         });
